0e140bac06dbdf32f2d4ab4b7e30d8c0b680a177
Matthew Bliffert <mattbliffert@icloud.com>
2018-06-12 23:12:45 -0400

    Added/updated tests.

--- src/components/tests/Layout.test.js
+++ src/components/tests/Layout.test.js
@@ -57,4 +57,56 @@ describe('<Layout />', () => {
       return testStyle(wrapperWithProps, style, value);
     });
   });
+
+  describe('with added props - 2nd set', () => {
+    const wrapperWithProps = shallow(
+      <Layout
+        paddingTop="40px"
+        paddingRight="30px"
+        paddingBottom="20px"
+        paddingLeft="10px"
+        marginTop="2px"
+        marginRight="4px"
+        marginBottom="6px"
+        marginLeft="8px"
+      />,
+    );
+
+    const expectedStyleList = [
+      { style: 'padding-top', value: '40px' },
+      { style: 'padding-right', value: '30px' },
+      { style: 'padding-bottom', value: '20px' },
+      { style: 'padding-left', value: '10px' },
+      { style: 'margin-top', value: '2px' },
+      { style: 'margin-right', value: '4px' },
+      { style: 'margin-bottom', value: '6px' },
+      { style: 'margin-left', value: '8px' },
+    ];
+
+    it('should match snapshot with props', () => {
+      expect(shallowToJson(wrapperWithProps)).toMatchSnapshot();
+    });
+
+    expectedStyleList.forEach(({ style, value }) => {
+      return testStyle(wrapperWithProps, style, value);
+    });
+  });
+
+  describe('with added props - left, right, centered', () => {
+    it('with left prop, should have style - margin-right: auto', () => {
+      const wrapper = shallow(<Layout left />);
+      expect(wrapper.find('div')).toHaveStyleRule('margin-right', 'auto');
+    });
+
+    it('with right prop, should have style - margin-left: auto', () => {
+      const wrapper = shallow(<Layout right />);
+      expect(wrapper.find('div')).toHaveStyleRule('margin-left', 'auto');
+    });
+
+    it('with centered prop, should have style - margin-left: auto, margin-right: auto', () => {
+      const wrapper = shallow(<Layout centered />);
+      expect(wrapper.find('div')).toHaveStyleRule('margin-left', 'auto');
+      expect(wrapper.find('div')).toHaveStyleRule('margin-right', 'auto');
+    });
+  });
 });
