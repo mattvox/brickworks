dbb6537f90632141e428b85afcf3c67a50139155
Matthew Bliffert <mattbliffert@icloud.com>
2018-06-25 13:36:03 -0400

    Remove column prop from Grid and add it to Row.

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,6 +1,5 @@
 import styled, { css } from 'styled-components';
 
-import media from '../utils/mediaTemplate';
 import applyCssWithUnit from '../utils/applyCssWithUnit';
 import { gridTypes, baseTypes } from './types';
 import { baseStyles } from './Base';
@@ -14,13 +13,6 @@ export const gridStyles = css`
   ${({
     theme,
     spacing,
-    columns,
-    xs,
-    sm,
-    md,
-    lg,
-    xl,
-    breakpoints,
     flow,
     justify,
     alignContent,
@@ -31,26 +23,6 @@ export const gridStyles = css`
     ${(theme.justify || justify) && css`justify-content: ${theme.justify || justify};`}
     ${(theme.alignContent || alignContent) && css`align-content: ${theme.alignContent || alignContent};`}
     ${(theme.alignItems || alignItems) && css`align-items: ${theme.alignItems || alignItems};`}
-    
-    ${(columns || xs) && css`
-      > .brckwrx-col {flex-basis: ${100 / (columns || xs)}%;}
-    `}
-
-    ${sm && media(theme.breakpoints || breakpoints).sm`
-      > .brckwrx-col {flex-basis: ${100 / sm}%;}
-    `}
-
-    ${md && media(theme.breakpoints || breakpoints).md`
-      > .brckwrx-col {flex-basis: ${100 / md}%;}
-    `}
-
-    ${lg && media(theme.breakpoints || breakpoints).lg`
-      > .brckwrx-col {flex-basis: ${100 / lg}%;}
-    `}
-    
-    ${xl && media(theme.breakpoints || breakpoints).xl`
-      > .brckwrx-col {flex-basis: ${100 / xl}%;}
-    `}
 
     ${(theme.noSpacing || noSpacing) || css`
       :not(.brckwrx-row) :not(.brckwrx-col) {
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,5 +1,6 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
+import media from '../utils/mediaTemplate';
 import { gridTypes, baseTypes } from './types';
 import { gridStyles } from './Grid';
 import { baseStyles } from './Base';
@@ -10,6 +11,28 @@ const Row = styled.div.attrs({ className: `brckwrx-row` })`
   flex: 0 1 100%;
   order: ${({ order }) => order || 0};
   ${gridStyles}
+
+  ${({ theme, columns, xs, sm, md, lg, xl, breakpoints }) => css`
+    ${(columns || xs) && css`
+      > .brckwrx-col {flex-basis: ${100 / (columns || xs)}%;}
+    `}
+
+    ${sm && media(theme.breakpoints || breakpoints).sm`
+      > .brckwrx-col {flex-basis: ${100 / sm}%;}
+    `}
+
+    ${md && media(theme.breakpoints || breakpoints).md`
+      > .brckwrx-col {flex-basis: ${100 / md}%;}
+    `}
+
+    ${lg && media(theme.breakpoints || breakpoints).lg`
+      > .brckwrx-col {flex-basis: ${100 / lg}%;}
+    `}
+    
+    ${xl && media(theme.breakpoints || breakpoints).xl`
+      > .brckwrx-col {flex-basis: ${100 / xl}%;}
+    `}
+  `};
 `;
 
 Row.propTypes = {
